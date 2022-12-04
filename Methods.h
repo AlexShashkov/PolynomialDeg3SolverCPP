be14611fc262a86b0e75cdb4f2a45bc3201d2523
AlexShashkov <sh@dmitrov-dubna.ru>
2022-12-04 16:42:10 +0300

    Minor changes

--- Methods.h
+++ Methods.h
@@ -13,7 +13,7 @@ using namespace std::complex_literals;
 
 namespace implementations{
 	/*Аргумент комплексного числа
-	@type int: complex<TEMPLATE>
+	@type inp: complex<TEMPLATE>
 	@param inp: Комплексное число.
 	@rtype: TEMPLATE
 	@returns: Аргумент комплексного числа.
@@ -31,22 +31,44 @@ namespace implementations{
 		}
 	}
 
+	/*FMS
+	@type a: TEMPLATE
+	@type b: TEMPLATE
+	@type c: TEMPLATE
+	@type d: TEMPLATE
+	@rtype:  TEMPLATE
+	*/
 	template <typename number>
 	number fms(number a, number b, number c, number d) {
 		auto tmp = d * c;
 		return std::fma(a, b, -tmp);
 	}
 
+	/*FMS
+	@type a: complex<TEMPLATE>
+	@type b: complex<TEMPLATE>
+	@type c: complex<TEMPLATE>
+	@type d: complex<TEMPLATE>
+	@rtype:  complex<TEMPLATE>
+	*/
 	template <typename number>
 	std::complex<number> fms(std::complex<number> a, std::complex<number> b, std::complex<number> c,std::complex<number> d) {
 		return {fms(a.real(),b.real(),c.real(),d.real())+fms(c.imag(),d.imag(),a.imag(),b.imag()),
 		fms(a.real(),b.imag(),c.real(),d.imag())+fms(b.real(),a.imag(),d.real(),c.imag())};
 	}
 
+	/*fused multiply-add for complex numbers
+	@type a: complex<TEMPLATE>
+	@type b: complex<TEMPLATE>
+	@type c: complex<TEMPLATE>
+	@returns: a*b+c
+	@rtype:  complex<TEMPLATE>
+	*/
 	template <typename number>
 	std::complex<number> fma(std::complex<number> a, std::complex<number> b, std::complex<number> c) {
 		return {fms(a.real(), b.real(), a.imag(), b.imag()) + c.real(),
-		fma(a.real(), b.imag(), fma(a.imag(), b.real(), c.imag()))};
+			fma(a.real(), b.imag(), fma(a.imag(), b.real(), c.imag()))
+		};
 	}
 
 	/*
@@ -195,7 +217,7 @@ namespace implementations{
 			number tmp1 = std::fma(static_cast<number>(2)*b0,c0,static_cast<number>(-3)*d0);
 
 			number o = std::fma(-b[1],std::fma(static_cast<number>(4)*b0,d0,-c[1]),fms(static_cast<number>(9)*d0,tmp1, static_cast<number>(4),c[2]));
-			number r = std::fma(one27,fms(static_cast<number>(2), b[2], static_cast<number>(9), b[0]*c[0]),d0);
+			number r = std::fma(one27,fms(static_cast<number>(2), b[2], static_cast<number>(9), b0*c0),d0);
 			number bthree = b0*onethree;
 
 			if(o == 0 && r == 0){
